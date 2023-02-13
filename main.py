c185cd3f59c5337ccb9052de109e413ce95eb729
Alen1407 <114245751+Alen1407@users.noreply.github.com>
2023-02-13 15:29:23 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import Tester
+import spreadsheet
 
 test = Tester.Tester()
 test.testAll()
+print()
+sp = spreadsheet.spreadsheet(3, 5)
+sp.setCellAt(2,1,"hey")
+sp.setCellAt(1,1,-5)
+sp.addRow(0)
+sp.addColumn(0)
+sp.removeRow(0)
+sp.printSpreadsheet()
